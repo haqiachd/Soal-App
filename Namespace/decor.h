9ff5ceb0ef39bc7631bc086f1e6aca64784ca6a2
Achmad Baihaqi <hakiahmad756@gmail.com>
2021-12-13 20:58:41 +0700

    Merapikan code

--- Namespace/decor.h
+++ Namespace/decor.h
@@ -11,13 +11,15 @@
 
 namespace decor{
 
-    using namespace std;
-    void gotoxy(int x, int y){
-COORD coord;
-coord.X = x;
-coord.Y = y;
-SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
+using namespace std;
+    
+void gotoxy(int x, int y){
+    COORD coord;
+    coord.X = x;
+    coord.Y = y;
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
+    
 void textcolor(int ForgC) {
      WORD wColor;
 
@@ -30,92 +32,97 @@ void textcolor(int ForgC) {
      }
      return;
  }
+    
 void Sound(int a){
     ///   ADD -lwinmm  in linker
     switch(a){
-case 1 :
-   PlaySound(TEXT("Database\\Sound\\Login.wav"),NULL,SND_SYNC);
-    break;
-case 2:
-    PlaySound(TEXT("Database\\Sound\\Logout.wav"),NULL,SND_SYNC);
-    break;
-case 3:
-    PlaySound(TEXT("Database\\Sound\\Success.wav"),NULL,SND_SYNC);
-    break;
-case 4:
-    PlaySound(TEXT("Database\\Sound\\notify.wav"),NULL,SND_SYNC);
-    break;
-case 5:
-    PlaySound(TEXT("Database\\Sound\\Next.wav"),NULL,SND_SYNC);
-    break;
-case 6:
-    PlaySound(TEXT("Database\\Sound\\Denied.wav"),NULL,SND_SYNC);
-    break;
-case 7:
-    PlaySound(TEXT("Database\\Sound\\Exit.wav"),NULL,SND_SYNC);
-    break;
-case 8:
-    PlaySound(TEXT("Database\\Sound\\Error.wav"),NULL,SND_SYNC);
-    break;
-    }
+        case 1 :
+           PlaySound(TEXT("Database\\Sound\\Login.wav"),NULL,SND_SYNC);
+            break;
+        case 2:
+            PlaySound(TEXT("Database\\Sound\\Logout.wav"),NULL,SND_SYNC);
+            break;
+        case 3:
+            PlaySound(TEXT("Database\\Sound\\Success.wav"),NULL,SND_SYNC);
+            break;
+        case 4:
+            PlaySound(TEXT("Database\\Sound\\notify.wav"),NULL,SND_SYNC);
+            break;
+        case 5:
+            PlaySound(TEXT("Database\\Sound\\Next.wav"),NULL,SND_SYNC);
+            break;
+        case 6:
+            PlaySound(TEXT("Database\\Sound\\Denied.wav"),NULL,SND_SYNC);
+            break;
+        case 7:
+            PlaySound(TEXT("Database\\Sound\\Exit.wav"),NULL,SND_SYNC);
+            break;
+        case 8:
+            PlaySound(TEXT("Database\\Sound\\Error.wav"),NULL,SND_SYNC);
+            break;
+            }
 }
 
 void randC (string randStr,int horizl, int vertkl,int lama,int akhir){
 
-     time_t t = time(NULL);
+    time_t t = time(NULL);
     tm* timePtr = localtime(&t);
 
     for (int i = 0; i <= akhir; i++){
+        int toValue = timePtr->tm_sec;
+        int RandClr = 1 + (rand() & 14);
+        int VarClr = 1 + (rand() & 14);
 
-  int toValue = timePtr->tm_sec;
-  int RandClr = 1 + (rand() & 14);
-  int VarClr = 1 + (rand() & 14);
-
-     textcolor(RandClr+VarClr);
-      gotoxy(horizl,vertkl);  cout << randStr <<endl;
+        textcolor(RandClr+VarClr);
+        gotoxy(horizl,vertkl);  cout << randStr <<endl;
         Sleep(lama);
-    textcolor(15);
-  }
+        textcolor(15);
+    }
 }
+    
 void writeText (string Text,int clrTxt, int sleepTxt, int horzntl, int vrtkl){
     int length = Text.length();
     char Teksch[length];
-strcpy(Teksch, Text.c_str());
+    
+    strcpy(Teksch, Text.c_str());
     textcolor(clrTxt);
-        gotoxy(horzntl,vrtkl);
+    gotoxy(horzntl,vrtkl);
+    
     for (int i = 0; i < length; i++){
         cout << Teksch[i];
             Sleep(sleepTxt);
     }
+    
     textcolor(15);
 }
+    
 void teksJalan_kanan (string NameStr,int beginLoc, int endLoc, int Nl_vertikal,int colorTxt,int speedJLN){
-int Mulai = beginLoc - NameStr.length(); // Mengambil posisi paling kanan
+    int Mulai = beginLoc - NameStr.length(); // Mengambil posisi paling kanan
     for (int i = Mulai; i >= endLoc; i--){
-             textcolor(colorTxt);
+        textcolor(colorTxt);
         gotoxy(i,Nl_vertikal); cout << NameStr <<" "<<endl;
-    Sleep(speedJLN);
+        Sleep(speedJLN);
     }
-
-textcolor(15);
+    textcolor(15);
 }
+    
 void teksJalan_kiri (string NameStr,int beginLoc,int endLoc, int Nl_vertikal,int colorTxt,int speedJLN){
     int Mulai = beginLoc;
-        for (int j = Mulai; j < endLoc; j++){
-               // system("cls");
-            textcolor(colorTxt);
-            gotoxy(j,Nl_vertikal);
-                cout <<" "<< NameStr <<endl;
-                Sleep(speedJLN);
-        }
+    for (int j = Mulai; j < endLoc; j++){
+        // system("cls");
+        textcolor(colorTxt);
+        gotoxy(j,Nl_vertikal);
+        cout <<" "<< NameStr <<endl;
+        Sleep(speedJLN);
+    }
         textcolor(15);
-
 }
+    
 void loading (int aw,int akr,int color, int Delay){
     for (;aw <= akr;aw++){
         textcolor(color);
-            cout <<"=";
-            Sleep(Delay);
+        cout <<"=";
+        Sleep(Delay);
     }
     textcolor(15);
 }
